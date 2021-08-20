d35cdd54003ff881c802836d30a0ee215056b97d
unknown <enginoobz@gmail.com>
2021-08-20 17:07:41 +0200

    Reformat project files due to new tab size

--- ts/Component.ts
+++ ts/Component.ts
@@ -1,10 +1,10 @@
 export abstract class Component {
-        public element: Element;
-        constructor(
-                protected id: string
-        ) {
-                this.element = document.querySelector('#' + id)!;
-        }
+  public element: Element;
+  constructor(
+    protected id: string
+  ) {
+    this.element = document.querySelector('#' + id)!;
+  }
 
-        // abstract onClick(): void;
+  // abstract onClick(): void;
 }
--- ts/basic/Button.ts
+++ ts/basic/Button.ts
@@ -3,19 +3,19 @@ import { Component } from "../Component.js";
 export const Button = (id: string) => new ButtonComponent(id);
 
 class ButtonComponent extends Component {
-        public label: string;
-        public onClick?: (event: Event) => void;
+  public label: string;
+  public onClick?: (event: Event) => void;
 
-        constructor(
-                id: string
-        ) {
-                super(id);
-                this.label = this.element.textContent!
-                this.onClick = undefined;
-                this.setupOnClick();
-        }
+  constructor(
+    id: string
+  ) {
+    super(id);
+    this.label = this.element.textContent!
+    this.onClick = undefined;
+    this.setupOnClick();
+  }
 
-        public setupOnClick() {
-                this.element.addEventListener('click', event => this.onClick!(event))
-        }
+  public setupOnClick() {
+    this.element.addEventListener('click', event => this.onClick!(event))
+  }
 }
